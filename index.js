70aa69498cdfb42b8ad258c05732364b10dee015
obemyshinnyrod <miswah.md@gmail.com>
2022-04-17 21:29:57 +0530

    (enh): added redislab support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const redis = require("redis");
+require("dotenv").config();
 
 // get sharejs dependencies
 const sharejs = require("share").server;
@@ -21,16 +22,31 @@ app.get("/", function (req, res) {
 //   res.render("pad");
 // });
 
-// set up redis server
-let redisClient;
-console.log(process.env.REDISTOGO_URL);
-if (process.env.REDISTOGO_URL) {
-  let rtg = require("url").parse(process.env.REDISTOGO_URL);
-  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-  redisClient.auth(rtg.auth.split(":")[1]);
-} else {
-  redisClient = require("redis").createClient();
-}
+// set up redis server for heroku
+// let redisClient;
+// console.log(process.env.REDISTOGO_URL);
+// if (process.env.REDISTOGO_URL) {
+//   let rtg = require("url").parse(process.env.REDISTOGO_URL);
+//   redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+//   redisClient.auth(rtg.auth.split(":")[1]);
+// } else {
+//   redisClient = require("redis").createClient();
+// }
+
+// Using Free tier db from redis lab
+const client = redis.createClient({
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+client.on("connect", () => {
+  console.log("Connected to our redis instance!");
+});
+
+client.on("error", (err) => {
+  global.console.log(err.message);
+});
 
 // options for sharejs
 const options = {
