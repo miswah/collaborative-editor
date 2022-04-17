bb1e3c170e8260e4272c2b79603fa18bdf76b8f1
obemyshinnyrod <miswah.md@gmail.com>
2022-04-17 20:58:06 +0530

    (enh): redis setup for heroku

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
   res.render("pad");
 });
-app.get("/(:id)", function (req, res) {
-  res.render("pad");
-});
+
+// Multiple files under progress
+// app.get("/(:id)", function (req, res) {
+//   res.render("pad");
+// });
+
+// set up redis server
+let redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  let rtg = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+  redisClient = require("redis").createClient();
+}
 
 // options for sharejs
 const options = {
